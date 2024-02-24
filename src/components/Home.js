6ab19e21b2ce9535dfa7e47d1af9eeb5005b0649
Heedo Kim <devheedoo@gmail.com>
2024-02-25 02:02:39 +0900

    feat: Load Marvel characters

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+/**
+ * @typedef Character
+ * @property {number} id
+ * @property {string} name
+ * @property {string} resourceURI
+ */
+
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [characters, setCharacters] = useState(/** @type {Character[]} */ ([]));
+
+  const loadMarvelCharacters = async () => {
+    await fetch(
+      "https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setCharacters(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(true);
+        console.error("Error while fetching data:", e);
+      });
+  };
+
+  useEffect(() => {
+    loadMarvelCharacters();
+  }, []);
+
   return (
     <div>
       <h1>Hello World</h1>
-      <Link to="/character/1">See character-1</Link>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <div>
+          {characters.map((character) => {
+            return (
+              <div>
+                {character.name}
+                <Link to={`/character/${character.id}`} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
