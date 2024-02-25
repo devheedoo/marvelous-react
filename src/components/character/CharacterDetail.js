bce83b8cf1f1bc8b570b824f633003586442d4e8
Heedo Kim <devheedoo@gmail.com>
2024-02-25 17:19:33 +0900

    feat: Styles character detail page

--- src/components/character/CharacterDetail.js
+++ src/components/character/CharacterDetail.js
@@ -1,5 +1,8 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useLoadCharacterDetail from "../../hooks/useLoadCharacterDetail";
+
+import commonStyles from "../common.module.css";
+import styles from "./CharacterDetail.module.css";
 
 const CharacterDetail = () => {
   const { id } = useParams();
@@ -7,20 +10,49 @@ const CharacterDetail = () => {
   const { detail, isLoading } = useLoadCharacterDetail({ id });
 
   return (
-    <div>
+    <div className={commonStyles.container}>
+      <h1 className={commonStyles.pageTitle}>
+        <Link to="/">MARVEL.characters</Link>
+      </h1>
       {isLoading && <p>Loading...</p>}
       {!isLoading && (
-        <div>
-          <h2>Hi, I'm {detail.name}</h2>
-          {detail.thumbnail.path}
-          {detail.urls.map((item, index) => {
-            return (
-              <ul key={index}>
-                <li>{item.type}</li>
-                <li>{item.url}</li>
+        <div className={styles.container}>
+          <div className={styles.profile}>
+            <img
+              src={`${detail.thumbnail.path}.${detail.thumbnail.extension}`}
+            />
+            <h2>{detail.name}</h2>
+          </div>
+
+          {/* comics, series, stories */}
+          <div className={styles.collectionContainer}>
+            <div className={styles.collection}>
+              <h3>COMICS</h3>
+              <ul>
+                {detail.comics.items.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
+                })}
               </ul>
-            );
-          })}
+            </div>
+
+            <div className={styles.collection}>
+              <h3>SERIES</h3>
+              <ul>
+                {detail.series.items.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
+                })}
+              </ul>
+            </div>
+
+            <div className={styles.collection}>
+              <h3>STORIES</h3>
+              <ul>
+                {detail.stories.items.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
         </div>
       )}
     </div>
